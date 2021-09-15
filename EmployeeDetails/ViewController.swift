240144b6bd324fd47d7faf0c03d977460850ab21
SiddhranDeviOS <siddhransiosdeveloper@gmail.com>
2021-09-15 15:46:45 +0530

    test commit background screen loading

--- EmployeeDetails/ViewController.swift
+++ EmployeeDetails/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemRed
+        let firstView = UIView()
+        firstView.frame = CGRect(x: self.view.bounds.width/2, y: self.view.bounds.height/2, width: 200, height: 50)
+        firstView.backgroundColor = .systemBlue
+        firstView.clipsToBounds = true
+        firstView.layer.cornerRadius = 8
+        firstView.center = self.view.center
+        self.view.addSubview(firstView)
     }
 
 
